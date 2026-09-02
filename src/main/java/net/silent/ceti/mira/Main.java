207f09588850cdd17979bcfd9d16a303e27814b5
Tomoko366 <taras130466@gmail.com>
2026-09-02 12:05:06 +0200

    add required initializators and shutdowners for DiscordBot and RoundTimer.

--- src/main/java/net/silent/ceti/mira/Main.java
+++ src/main/java/net/silent/ceti/mira/Main.java
@@ -1,7 +1,26 @@
 package net.silent.ceti.mira;
 
+import arc.ApplicationListener;
+import arc.Core;
 import mindustry.mod.Plugin;
+import net.silent.ceti.mira.bot.DiscordBot;
+import net.silent.ceti.mira.logger.MiraLogger;
+import net.silent.ceti.mira.time.RoundTimer;
 
 public class Main extends Plugin {
+    @Override
+    public void init() {
+        MiraLogger.info("Mira plugin loaded");
 
+        DiscordBot.init();
+
+        RoundTimer.init();
+
+        Core.app.addListener(new ApplicationListener() {
+            @Override
+            public void dispose() {
+                DiscordBot.shutdown();
+            }
+        });
+    }
 }
